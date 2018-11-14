824ec4f4551ec5db26d0b8125ea922cb20d02e33
amalvishnunk <amalvishnunk@gmail.com>
2018-11-14 14:08:23 +0530

    first

---
+++ 1.sh
@@ -0,0 +1,7 @@
+echo "# employee_databank" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git remote add origin https://github.com/amalvishnunk/employee_databank.git
+git push -u origin master
+echo "fisished"
